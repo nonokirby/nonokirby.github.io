f399b970f647f5851ee95e3ce1b7213a3bc68895
nonokirby <nonokirby@gmail.com>
2024-10-02 10:34:19 -0400

    add easy navbar mod

---
+++ Js/Navbar.js
@@ -0,0 +1,11 @@
+navBar = document.getElementById("NavBar");
+navBar.innerHTML =`
+<!-- Navbar -->
+<ul style="height: 46px;background-color: #333;">
+<ul>
+
+<li><a id="nindex" href="index.html">Home</a></li>
+<li><a id="ninformation" href="information.html">About</a></li>
+<li><a id="nlinks" href="links.html">Links</a></li>
+<li><a id="ntetris" href="tetris.html">Tetris</a></li>
+</ul></ul><!-- Navbar-->`
