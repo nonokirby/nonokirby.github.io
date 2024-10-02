21ae4e69aa839abd0ce5f971c96afc7293ac6050
nonokirby <nonokirby@gmail.com>
2024-10-02 10:41:46 -0400

    add navbar.js to all html files

--- Js/Navbar.js
+++ Js/Navbar.js
@@ -1,11 +1,10 @@
 navBar = document.getElementById("NavBar");
 navBar.innerHTML =`
 <!-- Navbar -->
-<ul style="height: 46px;background-color: #333;">
-<ul>
-
+<ul style="height: 46px;background-color: #333;"><ul>
 <li><a id="nindex" href="index.html">Home</a></li>
 <li><a id="ninformation" href="information.html">About</a></li>
 <li><a id="nlinks" href="links.html">Links</a></li>
 <li><a id="ntetris" href="tetris.html">Tetris</a></li>
+<li><a id="ntoast" href="toast.html">Toast</a></li>
 </ul></ul><!-- Navbar-->`
